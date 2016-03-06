d8d36002a56f69fff1e3cf8854cbdeac16f28d7a
Kazuo Sato <road2smileforall@gmail.com>
2016-03-06 14:05:12 +0900

    rebase for update listview layout.

--- app/src/main/java/satoshi/app/ideamosaic/sample/english/IdeaMosaicMenu.java
+++ app/src/main/java/satoshi/app/ideamosaic/sample/english/IdeaMosaicMenu.java
@@ -8,9 +8,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-
 import java.io.File;
 import java.io.IOException;
 
@@ -37,11 +34,6 @@ public class IdeaMosaicMenu extends AppCompatActivity implements OnClickListener
 
 		Layout_button();
 		createIdeaMosaicDB();
-
-
-		AdView mAdView = (AdView) findViewById(R.id.adView);
-		AdRequest adRequest = new AdRequest.Builder().build();
-		mAdView.loadAd(adRequest);
 
 	}
 
